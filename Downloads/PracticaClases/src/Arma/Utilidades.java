5302ba5d1f5f75213f557d2f88c4d26ebeb026c6
ortizzxz <104193784+ortizzxz@users.noreply.github.com>
2024-02-09 12:45:06 +0100

    JavaDocs de Utilidades

--- Downloads/PracticaClases/src/Arma/Utilidades.java
+++ Downloads/PracticaClases/src/Arma/Utilidades.java
@@ -1,13 +1,75 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Arma;
 
-/**
- *
- * @author franc
- */
 public class Utilidades {
+    
+    /**
+     * Muestra por pantalla los atributos públicos de la clase <code>Revolver</code>.
+     */
+    public static void consultaAtributosPublicos(){
+        
+        
+    }   
+    
+    
+     /**
+     * Muestra por pantalla el contenido de los atributos privados de la clase <code>Revolver</code>.
+     */
+    public static void consultaInformacionClase(){
+        
+        
+    }
+    
+    /**
+     * Muestra por pantalla el estado del objeto de la clase <code>Revolver</code>
+     * @param r objteo revólver cuyo estado se desea mostrar por pantalla
+     */
+    public static void consultaInformacionObjeto (Revolver r){
+        
+        
+    }
+    
+    /**
+     * Carga un revóler con un número de balas usando el método <code>cargar</code>
+     * de la clase <code>revolver</code>>.
+     * @param r objeto de la clase revolver que se va a cargar
+     * @param numBalas número de balas con las que se va a cargar
+     */
+    public static void cargarRevolver(Revolver r, int numBalas){
+        
+    }
+    
+    /**
+     * Carga un revóler completamente usando el método <code>cargar</code>
+     * de la clase <code>revolver</code>>.
+     * @param r objeto de la clase revolver que se va a cargar
+     */
+    public static void cargarRevolverCompletamente(Revolver r){
+        
+        
+    }
+    
+    
+    /**
+     * Dispara un revólver una cantidad de veces indicada por parámetro usando
+     * el método <code>disparar</code> de la clase <code>Revolver</code>.
+     * @param r objeto revolver que se va a disparar
+     * @param numDisparos cantidad de disparos que se desean realiar
+     */
+    public static void dispararRevolver(Revolver r, int numDisparos){
+        
+        
+    }
+    
+    
+     /**
+     * Descarga un revólver usando el método <code>descargar</code> de la clase 
+     * <code>Revovler</code>. Muestra por pantalla el estado antes y después de
+     * descargar.
+     * @param r objeto revolver que se va a descargar
+     */
+    public static void descargarRevolver(Revolver r){  
+        
+        
+    }
     
 }
