bd1290b1b3612fbd402576128cc704aa1adc7739
ortizzxz <104193784+ortizzxz@users.noreply.github.com>
2024-02-13 10:54:50 +0100

    JavaDoc del Programa

--- Downloads/PracticaClases/src/Arma/PruebaRevolver.java
+++ Downloads/PracticaClases/src/Arma/PruebaRevolver.java
@@ -5,16 +5,19 @@
 package Arma;
 
 /**
- *
- * @author franc
- */
+     * Programa prueba de la clase <code>Revólver</code>.
+     * El programa solicitará al usuario un número de revólveres entre 1 y 10.
+     * Una vez validada la entrada, se generará 1 revovler con capacidad por 
+     * defecto y el resto con capacidad aleatoria dentro de los márgenes definidos
+     * por la clase.
+     * A continuación, el programa solicitará un número de balas a cargar para
+     * el primer revovler. El resto se cargará completamente.
+     * Para finalizar, se pedirá al usuario un número de disparos a realizar por
+     * el primer revolver y se mostrará el resultado por pantalla.
+     * @param args
+     */
 public class PruebaRevolver {
     //
     public static void main(String[] args) {
-        Revolver r = new Revolver();
-        //
-        r.cargar(10);
-        r.descargar();
-        Utilidades.consultaInformacionObjeto(r);
     }
 }
