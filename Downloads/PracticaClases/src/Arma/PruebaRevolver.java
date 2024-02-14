1ee662ab2f51a36549f30bcda8eaf0894c64a004
ortizzxz <104193784+ortizzxz@users.noreply.github.com>
2024-02-14 12:45:40 +0100

    Desarrollo del programa - usuario. TAREAS PENDIENTES: - Acabar con la entrada de disparos a realizar y ejecutar los disparos validando la entrada del usuario y que el revolver tenga balas

--- Downloads/PracticaClases/src/Arma/PruebaRevolver.java
+++ Downloads/PracticaClases/src/Arma/PruebaRevolver.java
@@ -20,19 +20,80 @@ import java.util.Scanner;
      */
 public class PruebaRevolver {
     //
+    
+    public static final int MIN_REVOLVER = 1;
+    public static final int MAX_REVOLVER = 10;
+    
     public static void main(String[] args) {
+        
         //Atributos
-        int numR;
+        int numR = -1; // INICIALIZADA  A -1 PARA PODER INICIAR EL BUCLE WHILE
+        int numBalas = -1;
+        int numDisparos;
+        Revolver[] revolver;
         Scanner sc = new Scanner(System.in);
-        //
+        
+        // PRIMER MENSAJE DE PROGRAMA
         System.out.println("PROGRAMA PARA CREAR REVÓLVERES, ENTRE 1 Y 10.");
-        try{
-        System.out.print("¿Cúantos revólveres desea usted crear?\nNº: ");
-        numR = sc.nextInt();    
-        }catch(InputMismatchException e){
-            System.out.println("Debe ingresar un valor númerico.");
+        
+        while(numR < MIN_REVOLVER || numR > MAX_REVOLVER){
+            try{
+                //PRIMER WHILE PARA VALIDAR LA ENTRADA CORRECTA MIN_REVOLVER - MAX_REVOLVER
+                System.out.print("¿Cúantos revólveres desea usted crear?\nNº: ");
+                numR = sc.nextInt();  
+                
+                }catch(InputMismatchException e){
+                System.out.println("Debe ingresar un valor númerico. " + sc.next() + 
+                        " no es un número");
+            }
         }
-        //
+        
+        revolver = new Revolver[numR]; //ASIGNAMOS numR al tamaño del array.
+        
+        revolver[0] = new Revolver();
+        System.out.println("El primer revolver ha sigo generado con una capacidad "
+                + "por defecto.");
+        
+        // GENERAMOS EL RESTO CON CAPACIDAD ALEATORIA
+        for(int i = 1; i < revolver.length ; i++){
+            revolver[i] = new Revolver((int) (Math.random() * (Revolver.CAP_MAXIMA-Revolver.CAP_MINIMA+1) ) + (Revolver.CAP_MINIMA));
+        }
+                
+//        A continuación, el programa solicitará un número de balas a cargar para
+//     * el primer revovler. El resto se cargará completamente.
+
+
+        //Solicitamos numero de balas
+        System.out.println("Por favor, indícanos cúantas balas quieres cargar "
+                            + "para ael primer revolver - Ten en cuenta que el esto se cargara"
+                            + " completamente");
+        while(numBalas < Revolver.CAP_MINIMA || numBalas > Revolver.CAP_MAXIMA){ 
+            try{
+                if(numBalas < Revolver.CAP_MINIMA || numBalas > Revolver.CAP_MAXIMA){
+                    System.out.println("Recuerde que los valores minimos permitidso son " + Revolver.CAP_MINIMA
+                     + " y los valores maximos permitidos son: " + Revolver.CAP_MAXIMA);
+                }
+
+                numBalas = sc.nextInt();
+            }catch(InputMismatchException e){
+                System.out.println("Debe ingresar un valor númerico. " + sc.next() + 
+                        " no es un número");
+            }
+        }
+        
+        //EL RESTO SE CARGARA COMPLETAMENTE
+        for(int i = 1; i < revolver.length ; i++){
+            revolver[i].cargar();
+        }
+        
+        
+        System.out.println("Ahora y por ultimo, usted debera indicarnos la cantidad "
+                + "de veces que desea disparar su revolver.");
+        try{
+            numDisparos = sc.nextInt();
+        }catch(IllegalArgumentException e){
+            System.out.println(sc.next() + " no es un número entero.");
+        }
         
     }
 }
